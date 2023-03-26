ce1c1399f5b5802c68ac6e845a3f4aeb65e8c6e0
yuval-hazaz <hazaz.yuval@gmail.com>
2023-03-26 20:26:29 +0300

    commit changes

--- apps/server1/src/customer/customer.service.ts
+++ apps/server1/src/customer/customer.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { Customer, Prisma } from "@prisma/client";
 import { PrismaService } from "../prisma/prisma.service";
 import { CustomerServiceBase } from "./base/customer.service.base";
-
 @Injectable()
 export class CustomerService extends CustomerServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
+  }
+
+  async create<T extends Prisma.CustomerCreateArgs>(
+    args: Prisma.SelectSubset<T, Prisma.CustomerCreateArgs>
+  ): Promise<Customer> {
+    args.data.comments = "This customer created in Amplication server";
+    return super.create(args);
   }
 }
